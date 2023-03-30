a89b2fa2c78bc7d9ea87ea3161117cbb1d6ec997
kleber kunha <kleberkunha@hotmail.com>
2023-03-30 21:22:14 +0200

    sending contact form + config development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,17 +40,17 @@ Rails.application.configure do
   #config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: "smtp.gmail.com",
     port: 587,
-    domain: 'gmail.com',
-    authentication: 'plain',
+    domain: "gmail.com",
+    authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD']
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
 
   config.action_mailer.perform_caching = false
