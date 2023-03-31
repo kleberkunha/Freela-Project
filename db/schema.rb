e5dbb9f9ba76e48377cb615fd85d869780be816d
kleber kunha <kleberkunha@hotmail.com>
2023-03-31 19:14:49 +0200

    remake db

--- db/schema.rb
+++ db/schema.rb
@@ -100,8 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_122734) do
     t.text "service4"
     t.text "member"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"],
-            name: "index_users_on_reset_password_token",
-            unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
