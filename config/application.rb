5e14044f7ea39ac02203964f37759409d92ac51f
kleber kunha <kleberkunha@hotmail.com>
2023-03-31 17:44:44 +0200

    change app.rb

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module LineimarBlog
+module completeblog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
